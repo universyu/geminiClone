1af70d14e617525d08ae9e6c556f250d3013b483
universyu <Lzy500429@gmail.com>
2024-05-30 20:20:24 +0800

    访问服务器443端口

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,6 +1,6 @@
 
 async function run(prompt) {
-    const response = await fetch('https://10.249.8.149:3002/generate', {
+    const response = await fetch('https://10.249.8.149:443/generate', {
         method: 'POST',
         mode: 'cors',
         headers: {
